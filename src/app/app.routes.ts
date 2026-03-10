c83ebbe42d1155a62b23e904ea301a2d75227b42
Blayzzand <368833@edu.itmo.ru>
2026-03-10 20:03:01 +0300

    initiate project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Items } from './pages/items/items';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'items', component: Items },
+];
